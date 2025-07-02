c21b2c4e49875190603cd9e2b4a55ead39a71490
Odin Asbjørnsen <odinas004@gmail.com>
2025-07-02 17:45:49 +0200

    Add screen with text field for link validation

--- composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/App.kt
+++ composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/App.kt
@@ -1,43 +1,73 @@
 package com.terrydroid.msgverify
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.terrydroid.msgverify.di.commonModule
 import com.terrydroid.msgverify.home.HomeViewModel
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
 import org.koin.compose.viewmodel.koinViewModel
-import org.koin.core.KoinApplication
 
 @Composable
 fun App() {
     KoinApplication(application = {
         modules(commonModule())
     }) {
-        MaterialTheme {
-            Column(
-                modifier = Modifier
-                    .safeContentPadding()
-                    .fillMaxSize(),
-                horizontalAlignment = Alignment.CenterHorizontally,
-            ) {
-                val vm : HomeViewModel = koinViewModel()
+        AppScreen()
+    }
+}
 
-                Scaffold(
-                    contentWindowInsets = WindowInsets(0.dp)
-                ) { innerPadding ->
-                    Column(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
+@Composable
+private fun AppScreen() {
+    MaterialTheme {
+        Column(
+            modifier = Modifier
+                .safeContentPadding()
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            val vm: HomeViewModel = koinViewModel()
 
+            Scaffold(
+                contentWindowInsets = WindowInsets(0.dp)
+            ) { innerPadding ->
+                Column(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
+                    Spacer(modifier = Modifier.size(160.dp))
+                    val textFieldValue = remember { mutableStateOf("") }
+                    OutlinedTextField(
+                        modifier = Modifier.fillMaxWidth(80f),
+                        value = textFieldValue.value,
+                        onValueChange = {
+                            textFieldValue.value = it
+                        },
+                        placeholder = {
+                            Text("Enter Url you want to verify")
+                        }
+                    )
+
+                    Button(
+                        onClick = {
+                            vm.onVerifyClicked(textFieldValue.value)
+                        }
+                    ) {
+                        Text("Verify")
                     }
                 }
             }
@@ -49,6 +79,6 @@ fun App() {
 @Composable
 private fun AppPreview() {
     MaterialTheme {
-        App()
+        AppScreen()
     }
 }
--- composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/home/HomeViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/home/HomeViewModel.kt
@@ -2,14 +2,34 @@ package com.terrydroid.msgverify.home
 
 import androidx.lifecycle.ViewModel
 import com.terrydroid.msgverify.data.MsgVerifyRepository
-import org.koin.core.component.inject
-import kotlin.getValue
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
 
 class HomeViewModel(
-   msgVerifyRepository: MsgVerifyRepository
-): ViewModel() {
+    msgVerifyRepository: MsgVerifyRepository
+) : ViewModel() {
 
+    private val _linkVerificationState: MutableStateFlow<LinkVerificationState> = MutableStateFlow(
+        LinkVerificationState.Idle
+    )
 
+    val linkVerificationState: StateFlow<LinkVerificationState>
+        get() = _linkVerificationState.asStateFlow()
+
+    fun onVerifyClicked(inputLink: String) {
+
+        //TODO: Do verification that it is actually a link
+
+    }
+
+}
+
+sealed class LinkVerificationState {
+    data object Idle : LinkVerificationState()
+    data object LoadingVerification : LinkVerificationState()
+    data class Error(val errorMessage: String) : LinkVerificationState()
+    data class Success(val linkMaliciousPercentage: String) : LinkVerificationState()
 }
 
