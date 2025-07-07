3ef258590f6139c571764a1c62dc14dd332b04a7
terry-brett <85226033+terry-brett@users.noreply.github.com>
2025-07-07 18:11:59 +0200

    configure gradle for the library

---
+++ ContextGuard/settings.gradle.kts
@@ -0,0 +1,35 @@
+rootProject.name = "ContextGuard"
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+        mavenLocal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        google()
+        mavenCentral()
+        mavenLocal()
+    }
+}
+
+include(":library")
+
