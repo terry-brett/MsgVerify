bd8a01e8923b41255fc7b34d259989ee706e77cc
Odin Asbjørnsen <odinas004@gmail.com>
2025-07-13 01:44:58 +0200

    Improve historical links UI

--- composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/home/components/HistoricalLinkResults.kt
+++ composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/home/components/HistoricalLinkResults.kt
@@ -1,8 +1,6 @@
 package com.terrydroid.msgverify.home.components
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -13,50 +11,51 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Error
 import androidx.compose.material.icons.filled.Shield
 import androidx.compose.material.icons.filled.Warning
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.terrydroid.msgverify.home.ClassificationColor
 import com.terrydroid.msgverify.home.LinkResult
 import com.terrydroid.msgverify.home.LinkVerificationState
 
 internal fun LazyListScope.historicalLinkResults(
-    linkVerificationState: LinkVerificationState,
-    onShowBottomSheet: (link: LinkResult) -> Unit
+    linkVerificationState: LinkVerificationState, onShowBottomSheet: (link: LinkResult) -> Unit
 ) {
     items(linkVerificationState.verifiedLinkHistory) { item ->
-        Row(
-            modifier = Modifier.fillMaxWidth().clickable(onClick = {
-                onShowBottomSheet.invoke(item)
-            }),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceBetween
-        ) {
-            Text(
-                text = item.url,
-                color = MaterialTheme.colorScheme.onBackground,
-                style = MaterialTheme.typography.bodyMedium
-            )
-            Spacer(modifier = Modifier.size(8.dp))
-            Row(
-                verticalAlignment = Alignment.CenterVertically
+        val icon = when (item.classificationColor) {
+            ClassificationColor.Red -> Icons.Default.Error
+            ClassificationColor.Yellow -> Icons.Default.Warning
+            ClassificationColor.Green -> Icons.Default.Shield
+        }
+        Spacer(Modifier.size(8.dp))
+        ElevatedCard(onClick = {
+            onShowBottomSheet.invoke(item)
+        }) {
+            Column(
+                modifier = Modifier.padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                val icon = when (item.classificationColor) {
-                    ClassificationColor.Red -> Icons.Default.Error
-                    ClassificationColor.Yellow -> Icons.Default.Warning
-                    ClassificationColor.Green -> Icons.Default.Shield
-                }
                 Icon(
-                    modifier = Modifier.padding(4.dp),
-                    imageVector = icon,
-                    contentDescription = null
+                    modifier = Modifier.padding(4.dp), imageVector = icon, contentDescription = null
                 )
                 Text(
-                    text = "${item.linkMaliciousPercentage} %",
-                    color = MaterialTheme.colorScheme.onBackground
+                    modifier = Modifier.fillMaxWidth(),
+                    text = item.url,
+                    color = MaterialTheme.colorScheme.onBackground,
+                    style = MaterialTheme.typography.bodyMedium,
+                    textAlign = TextAlign.Center
+                )
+
+                Text(
+                    modifier = Modifier.fillMaxWidth().padding(top = 8.dp),
+                    text = "Malicious Chance Percentage: ${item.linkMaliciousPercentage} %",
+                    color = MaterialTheme.colorScheme.onBackground,
+                    textAlign = TextAlign.Center
                 )
             }
         }
