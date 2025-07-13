db6fe17182eb7eabd63241c73abb940d2efeaca4
Odin Asbjørnsen <odinas004@gmail.com>
2025-07-13 18:14:31 +0200

    Add a top appbar with actions
    Add a top app bar with action that will be implemented in the future

--- composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/App.kt
+++ composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/App.kt
@@ -5,19 +5,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import com.terrydroid.msgverify.di.commonModule
 import com.terrydroid.msgverify.home.HomeScreen
-import com.terrydroid.msgverify.home.HomeViewModel
 import com.terrydroid.msgverify.theme.MsgVerifyTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.koin.compose.KoinApplication
-import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 fun App() {
@@ -28,6 +36,7 @@ fun App() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun AppScreen() {
     MsgVerifyTheme {
@@ -38,8 +47,36 @@ private fun AppScreen() {
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
+            val scrollBehavior =
+                TopAppBarDefaults.exitUntilCollapsedScrollBehavior(rememberTopAppBarState())
+
             Scaffold(
                 contentWindowInsets = WindowInsets(0.dp),
+                topBar = {
+                    CenterAlignedTopAppBar(
+                        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+                        title = {
+                            Text(
+                                text = "MsgVerify",
+                                style = MaterialTheme.typography.headlineMedium,
+                                color = MaterialTheme.colorScheme.onBackground,
+                            )
+                        },
+                        scrollBehavior = scrollBehavior,
+                        actions = {
+                            IconButton(
+                                onClick = {
+                                    // TODO: Navigate to settings
+                                }
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.Settings,
+                                    contentDescription = "Go to settings"
+                                )
+                            }
+                        }
+                    )
+                }
             ) { innerPadding ->
                 HomeScreen(
                     paddingValues = innerPadding,
