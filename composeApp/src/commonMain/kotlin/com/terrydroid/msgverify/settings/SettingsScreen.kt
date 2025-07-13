55ec28432f6d46d2f52df44426803fde6b25ed9d
Odin Asbjørnsen <odinas004@gmail.com>
2025-07-13 18:50:41 +0200

    Add initial settings screen with navigation
    Just add state navigation for now to navigate to empty settings screen.
    When the app gets more complext we might want to improve the navigation
    a bit.

---
+++ composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/settings/SettingsScreen.kt
@@ -0,0 +1,11 @@
+package com.terrydroid.msgverify.settings
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+
+@Composable
+internal fun SettingsScreen(
+    paddingValues: PaddingValues
+) {
+
+}
