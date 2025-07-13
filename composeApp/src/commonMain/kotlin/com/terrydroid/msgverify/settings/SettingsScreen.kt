3efbd34397e5dbd04a108f5cbb069d4a9c8c09bb
Odin Asbjørnsen <odinas004@gmail.com>
2025-07-13 20:58:00 +0200

    Add start UI for settings overlay

--- composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/settings/SettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/settings/SettingsScreen.kt
@@ -1,11 +1,80 @@
 package com.terrydroid.msgverify.settings
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import org.koin.compose.viewmodel.koinViewModel
 
 @Composable
 internal fun SettingsScreen(
-    paddingValues: PaddingValues
+    paddingValues: PaddingValues,
+    settingsViewModel: SettingsViewModel = koinViewModel()
 ) {
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = paddingValues
+    ) {
+        item {
+            Spacer(Modifier.size(24.dp))
+        }
+        item {
+            Text(
+                text = "Overlay settings",
+                style = MaterialTheme.typography.labelSmall
+            )
+        }
+        item {
+            Spacer(Modifier.size(8.dp))
+        }
+        item {
+            val overlayEnabled =
+                remember { mutableStateOf(false) } // TODO: Get default state from system
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .clip(shape = MaterialTheme.shapes.small)
+                    .background(color = MaterialTheme.colorScheme.surfaceContainer)
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Column {
+                    Text(
+                        text = "Enable overlay on other apps",
+                        style = MaterialTheme.typography.labelLarge
+                    )
+                    Text(
+                        text = "Will demand system settings enabled",
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
+                Checkbox(
+                    checked = overlayEnabled.value,
+                    onCheckedChange = { checked ->
+                        overlayEnabled.value = checked
+                    }
+                )
+            }
+        }
+
+    }
 
 }
---
+++ composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/settings/SettingsViewModel.kt
@@ -0,0 +1,7 @@
+package com.terrydroid.msgverify.settings
+
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel(): ViewModel() {
+
+}
