d5f6662cdbb26249ceb1e7254c0257ddeae1ab1c
terry-brett <85226033+terry-brett@users.noreply.github.com>
2025-11-16 10:45:22 +0100

    add message classification model to context guard

--- ContextGuard/library/src/androidMain/kotlin/org/contextguard/lib/MLKit/messageClassification/LoadModel.kt
+++ ContextGuard/library/src/androidMain/kotlin/org/contextguard/lib/MLKit/messageClassification/LoadModel.kt
@@ -1,9 +1,17 @@
 package org.contextguard.lib.MLKit.messageClassification
 
+import android.content.Context
 import dev.kursor.ktensorflow.api.ModelDesc
+import org.contextguard.lib.library.generated.resources.Res
+import java.io.File
 
-actual class LoadModel {
+actual class LoadModel(private val context: Context) {
     actual suspend fun getTextModelDesc(): ModelDesc {
-        TODO("Not yet implemented")
+        val bytes = Res.readBytes("files/messageClassificationModel/distilbert_mnli_dynamic_quant.tflite")
+        val tmpFile = File.createTempFile("prefix", "suffix", context.cacheDir)
+        tmpFile.writeBytes(bytes)
+        val modelDesc = ModelDesc.File(tmpFile)
+
+        return modelDesc
     }
 }
--- ContextGuard/library/src/iosMain/kotlin/org/contextguard/lib/MLKit/messageClassification/LoadModel.kt
+++ ContextGuard/library/src/iosMain/kotlin/org/contextguard/lib/MLKit/messageClassification/LoadModel.kt
@@ -1,9 +1,14 @@
 package org.contextguard.lib.MLKit.messageClassification
 
 import dev.kursor.ktensorflow.api.ModelDesc
+import org.contextguard.lib.library.generated.resources.Res
 
 actual class LoadModel {
     actual suspend fun getTextModelDesc(): ModelDesc {
-        TODO("Not yet implemented")
+        val modelDesc = ModelDesc.PathInBundle(
+            Res.getUri("files/messageClassificationModel/distilbert_mnli_dynamic_quant.tflite")
+                .removePrefix("file://")
+        )
+        return modelDesc
     }
 }
