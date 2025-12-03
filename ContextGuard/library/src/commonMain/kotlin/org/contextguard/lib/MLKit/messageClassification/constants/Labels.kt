f3fd5dce7a8b77b0397672de944ff3bd42781fc0
terry-brett <85226033+terry-brett@users.noreply.github.com>
2025-12-03 16:20:56 +0100

    update labels

--- ContextGuard/library/src/commonMain/kotlin/org/contextguard/lib/MLKit/messageClassification/constants/Labels.kt
+++ ContextGuard/library/src/commonMain/kotlin/org/contextguard/lib/MLKit/messageClassification/constants/Labels.kt
@@ -4,10 +4,9 @@ object Labels {
     val reasonLabels = listOf(
         "Spelling Errors",
         "Urgency",
-        "Statement of Warning",
+        "Intimidation",
         "Sensitive Information Request",
         "Impersonation",
-        "Suspicious Attachments",
         "Too Good To Be True",
         "Payment Request",
         "Emotional Manipulation",
@@ -18,7 +17,6 @@ object Labels {
         "Credential Verification Request",
         "Unusual Account Activity Claim",
         "Poor Formatting",
-        "Mismatched URLs",
         "Link Click Pressure"
     )
 }
