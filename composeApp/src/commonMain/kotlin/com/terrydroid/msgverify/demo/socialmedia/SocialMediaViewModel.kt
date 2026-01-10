ca362121d927bb6f2af27a9d42e920b53fdd0b73
Odin Asbjørnsen <odinas004@gmail.com>
2026-01-10 16:16:50 +0100

    Connect the social media demo with actuall repository

--- composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/demo/socialmedia/SocialMediaViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/demo/socialmedia/SocialMediaViewModel.kt
@@ -1,14 +1,18 @@
 package com.terrydroid.msgverify.demo.socialmedia
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.terrydroid.msgverify.data.MsgVerifyRepository
 import com.terrydroid.msgverify.demo.smsoverview.TrafficLight
+import com.terrydroid.msgverify.demo.smsoverview.getClassification
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class SocialMediaViewModel constructor(
+class SocialMediaViewModel(
     private val msgVerifyRepository: MsgVerifyRepository,
     dispatcher: CoroutineDispatcher
 ): ViewModel() {
@@ -17,6 +21,41 @@ class SocialMediaViewModel constructor(
     val state: StateFlow<Messages>
         get() = _state.asStateFlow()
 
+    init {
+        viewModelScope.launch(dispatcher) {
+            state.value.messages.forEach { message ->
+                msgVerifyRepository.verifyContent(message.message).collect { result ->
+                    result.fold(
+                        onFailure = {},
+                        onSuccess = { value ->
+                            val maliciousScorePercent = value.maliciousScore
+
+                            val classification = getClassification(maliciousScorePercent)
+                            _state.update {
+                                Messages(
+                                    it.messages.replace(
+                                        Message(
+                                            id = message.id,
+                                            title = message.title,
+                                            message = message.message,
+                                            trafficLight = classification
+                                        )
+                                    ) { value ->
+                                        value == message
+                                    }
+                                )
+                            }
+                        }
+                    )
+                }
+            }
+        }
+    }
+}
+private fun <T> List<T>.replace(newValue: T, block: (T) -> Boolean): List<T> {
+    return map {
+        if (block(it)) newValue else it
+    }
 }
 data class Messages(
     val messages: List<Message>,
