7d4966c6827eff5216996830cdcd6c6493e9ca0e
terry-brett <85226033+terry-brett@users.noreply.github.com>
2026-01-10 12:52:51 +0100

    Update Labels.kt

--- ContextGuard/library/src/commonMain/kotlin/org/contextguard/lib/MLKit/messageClassification/constants/Labels.kt
+++ ContextGuard/library/src/commonMain/kotlin/org/contextguard/lib/MLKit/messageClassification/constants/Labels.kt
@@ -10,7 +10,6 @@ object Labels {
         "Link Click Pressure",
         "Financial/Personal Information Request", // bank account, home address, sort-code, card details, name, social security
         "Too Good To Be True",
-        "Generic Greeting",
         "Credential Verification Request",
     )
 }
