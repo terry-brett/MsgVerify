41611b574f8dc2f0d2c4c50fa8d6d31478ab4543
ama-yash <yash.soni5999@gmail.com>
2026-01-16 19:50:36 +0000

    remove obsolete heuristic Kotlin files

--- ContextGuard/library/src/commonMain/kotlin/org/contextguard/lib/heuristics/labelHelper.kt
+++
@@ -1,9 +0,0 @@
-package org.contextguard.lib.heuristics.messageLabelling
-
-fun normalise(message: String): String {
-    return message
-        .lowercase()
-        .replace(Regex("\\s+"), " ")
-        .trim()
-}
-
