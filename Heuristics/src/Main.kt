2faee2f3c34be7bf85906a9b132fdb21279baedd
terry-brett <85226033+terry-brett@users.noreply.github.com>
2026-01-17 10:15:12 +0100

    move heuristics to contextguard

--- Heuristics/src/Main.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-
-    val message = "Hey Yash!, your account has been locked"
-    val sender = "yash.soni@hmrc.com"
-
-    val labeller = MessageReasoningLabels(message, sender)
-
-    println(labeller.addLabels())
-}
