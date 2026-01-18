e6c3e754c620538908cf4f11f6cde1c9a20104ea
Odin Asbjørnsen <odinas004@gmail.com>
2026-01-18 15:36:09 +0100

    Update repository with new API implementations

--- composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/data/MsgVerifyRepository.kt
+++ composeApp/src/commonMain/kotlin/com/terrydroid/msgverify/data/MsgVerifyRepository.kt
@@ -1,50 +1,63 @@
 package com.terrydroid.msgverify.data
 
 import com.terrydroid.msgverify.PlatformContext
+import kotlin.collections.firstOrNull
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
-import org.contextguard.lib.MLKit.urlClassification.UrlPrediction
-import org.contextguard.lib.usecase.CalculateResultUseCase
+import org.contextguard.lib.ContentVerifierImpl
+import org.contextguard.models.Reason
+import org.contextguard.models.TextClassificationResult
 import org.koin.core.component.KoinComponent
-
 
 class MsgVerifyRepository(val platformContext: PlatformContext) : KoinComponent {
 
-    suspend fun verifyLink(url: String): Flow<Result<LinkVerificationResponse>> {
-        val result = UrlPrediction(platformContext.getNativeContext()).makePrediction(url)
-        return flowOf(
-            Result.success(
-                LinkVerificationResponse(
-                    maliciousScore = result,
-                    description = ""
-                )
-            )
-        )
+  suspend fun verifyLink(url: String): Flow<Result<LinkVerificationResponse>> {
+    val result = ContentVerifierImpl(platformContext).verifyUrl(url)?.urlScores?.firstOrNull() ?: 0f
+
+    return flowOf(
+        Result.success(LinkVerificationResponse(maliciousScore = result, description = ""))
+    )
+  }
+
+  suspend fun verifyContent(
+      input: String,
+      sender: String,
+  ): Flow<Result<ContentVerificationResponse>> {
+    val result = ContentVerifierImpl(platformContext).verify(content = input, sender = sender)
+
+    if (result == null) {
+      return flowOf(Result.failure(Exception("Unable to verify content")))
     }
 
-    suspend fun verifyContent(input: String): Flow<Result<LinkVerificationResponse>> {
-        val result = CalculateResultUseCase(message = input, sender = null, platformContext = platformContext.getNativeContext()).invoke()
-        // TODO: Connect this when it becomes available
-        result.textClassificationResult
-        return flowOf(
+    return when (result.textClassificationResult) {
+      is TextClassificationResult.Unsafe -> {
+        flowOf(
             Result.success(
-                LinkVerificationResponse(
-                    maliciousScore = result.urlScore?.toFloat() ?: 0f,
-                    description = result.textClassificationResult.toString()
+                ContentVerificationResponse.Unsafe(
+                    result.urlScores,
+                    result.textClassificationResult.listOfReasons,
                 )
             )
         )
+      }
+      is TextClassificationResult.Safe -> {
+        flowOf(Result.success(ContentVerificationResponse.Safe))
+      }
     }
+  }
 }
 
 /**
- * After asking the machine learning model if the link is malicious,
- * it will return a score.
- * This is the @property maliciousScore. This is a percentage 0 - 1
+ * After asking the machine learning model if the link is malicious, it will return a score. This is
+ * the @property maliciousScore. This is a percentage 0 - 1
  *
  * @property description - Description of why the model thinks it has this score
  */
-data class LinkVerificationResponse(
-    val maliciousScore: Float,
-    val description: String
-)
+data class LinkVerificationResponse(val maliciousScore: Float, val description: String)
+
+sealed class ContentVerificationResponse {
+  data object Safe : ContentVerificationResponse()
+
+  data class Unsafe(val urlScores: List<Float>?, val reasons: List<Reason>) :
+      ContentVerificationResponse()
+}
